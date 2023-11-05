fa5154d0ae50e6b754258f87147050f3a36de6f8
Irshad <irshad9in@gmail.com>
2023-11-06 00:17:36 +0100

    First working implementation

--- Resource.h
+++ Resource.h
@@ -7,30 +7,22 @@
 #define IDD_WIN32APIEXPERIMENTS_DIALOG  102
 #define IDD_PROPPAGE_SMALL              102
 #define IDS_APP_TITLE                   103
-#define IDD_ABOUTBOX                    103
 #define IDM_ABOUT                       104
-#define IDM_EXIT                        105
-#define IDI_WIN32APIEXPERIMENTS         107
-#define IDI_SMALL                       108
 #define IDC_WIN32APIEXPERIMENTS         109
 #define IDR_MAINFRAME                   128
 #define IDR_ACCELERATOR1                129
-#define IDB_BITMAP1                     130
-#define IDB_BITMAP2                     131
 #define IDD_DIALOG1                     132
 #define IDB_PNG1                        134
 #define IDB_BITMAP6                     138
-#define IDB_BITMAP3                     139
-#define IDB_BITMAP4                     140
-#define IDB_BITMAP5                     141
-#define IDB_BITMAP7                     142
-#define IDB_BITMAP8                     143
-#define IDB_BITMAP9                     144
-#define IDB_BITMAP10                    145
-#define IDB_BITMAP11                    146
+#define IDB_SEG2x                       148
+#define IDB_SEG3                        149
+#define IDB_SEG4                        150
+#define IDB_SST1A                       152
 #define IDC_seg0                        1000
 #define IDC_seg1                        1001
 #define IDC_SEGx                        1002
+#define IDC_seg2                        1005
+#define IDC_seg3                        1006
 #define IDC_STATIC                      -1
 
 // Next default values for new objects
@@ -38,7 +30,7 @@
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NO_MFC                     1
-#define _APS_NEXT_RESOURCE_VALUE        147
+#define _APS_NEXT_RESOURCE_VALUE        153
 #define _APS_NEXT_COMMAND_VALUE         32772
 #define _APS_NEXT_CONTROL_VALUE         1003
 #define _APS_NEXT_SYMED_VALUE           110
--- Win32_API_experiments.cpp
+++ Win32_API_experiments.cpp
@@ -7,178 +7,92 @@
 
 #include "framework.h"
 #include "Win32_API_experiments.h"
-#include "qwin_gui.h"
-
-LRESULT CALLBACK AboutDlgProc(HWND hwnd, UINT Message, WPARAM wParam, LPARAM lParam);
-
-LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
 
 static HINSTANCE l_hInst;   /* this application instance */
+static HWND      l_hWnd;    /* main window handle */
+static LPSTR     l_cmdLine; /* the command line string */
 
-int WINAPI WinMain(
-    _In_ HINSTANCE hInstance,
-    _In_opt_ HINSTANCE hPrevInstance,
-    _In_ LPSTR lpCmdLine,
-    _In_ int nShowCmd
-)
+static int l_paused;
+
+/* Local functions ---------------------------------------------------------*/
+static LRESULT CALLBACK WndProc(HWND hWnd, UINT iMsg,
+    WPARAM wParam, LPARAM lParam);
+
+/*..........................................................................*/
+int WINAPI WinMain(HINSTANCE hInst, HINSTANCE hPrevInst,
+    LPSTR cmdLine, int iCmdShow)
 {
+    l_hInst = hInst;
+    WNDCLASSEX wndclass;
 
-    const wchar_t class_name[] = L"First window class";
+    wndclass.cbSize = sizeof(wndclass);
+    wndclass.style = CS_HREDRAW | CS_VREDRAW;
+    wndclass.lpfnWndProc = WndProc;
+    wndclass.cbClsExtra = 0;
+    wndclass.cbWndExtra = DLGWINDOWEXTRA;
+    wndclass.hInstance = hInst;
+    wndclass.hIcon = NULL;
+    wndclass.hCursor = LoadCursor(NULL, IDC_ARROW);
+    wndclass.hbrBackground = (HBRUSH)COLOR_WINDOW;
+    wndclass.lpszMenuName = NULL;
+    wndclass.lpszClassName = L"First";
+    wndclass.hIconSm = NULL;
 
-    WNDCLASS wc = {};
+    RegisterClassEx(&wndclass);
 
-    wc.lpfnWndProc = WindowProc;
-    wc.hInstance = hInstance;
-    wc.lpszClassName = class_name;
+    return DialogBox(hInst, MAKEINTRESOURCE(IDD_DIALOG1), NULL, WndProc);
+}
 
-    l_hInst = hInstance;
 
-    RegisterClass(&wc);
+static LRESULT CALLBACK WndProc(HWND hWnd, UINT iMsg,
+    WPARAM wParam, LPARAM lParam)
+{
+    switch (iMsg) {
 
-    HWND hwnd = CreateWindowExW(
-        0,
-        class_name,
-        L"Learning project",
-    WS_OVERLAPPEDWINDOW,
-    CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT,
-        NULL,
-        NULL,
-        hInstance,
-        NULL
-    );
-   
+    case WM_CREATE: {
+        l_hWnd = hWnd; /* save the window handle */
 
-    if (hwnd == NULL)return 0;
-
-    ShowWindow(hwnd, nShowCmd);
-
-    MSG msg = {};
-
-    while (GetMessage(&msg, NULL, 0, 0) > 0) {
-        TranslateMessage(&msg);
-        DispatchMessageW(&msg);
-
+        /* initialize the owner-drawn buttons...
+        * NOTE: must be done *before* the first drawing of the buttons,
+        * so WM_INITDIALOG is too late.
+        */
+        return 0;
     }
-    return 0;
-}
+    case WM_COMMAND: {
+        switch (wParam) {
+        case IDOK:
+        {
 
-static SegmentDisplay   l_scoreBoard; /* segment display for the score */
+            // HBITMAP hBitmap = (HBITMAP)::LoadImage(NULL, L"bitmap7.bmp", IMAGE_BITMAP,0, 0, LR_LOADFROMFILE | LR_DEFAULTSIZE);
 
+            HBITMAP hBitmap = LoadBitmap(GetModuleHandle(NULL), MAKEINTRESOURCE(IDB_SEG2x));
+            SendMessage(GetDlgItem(hWnd, IDC_seg0), STM_SETIMAGE,
+                (WPARAM)IMAGE_BITMAP, (LPARAM)hBitmap);
+            SendMessage(GetDlgItem(hWnd, IDC_seg1), STM_SETIMAGE,
+                (WPARAM)IMAGE_BITMAP, (LPARAM)hBitmap);
 
-static int count = 0;
-static void update_seg(int n) {
-    /* update the score in the l_scoreBoard SegmentDisplay */
-    SegmentDisplay_setSegment(&l_scoreBoard, 0U, n++);
-    n /= 10U;
-    SegmentDisplay_setSegment(&l_scoreBoard, 1U, n++);
-    n /= 10U;
-}
+        }
+            break;
 
-LRESULT CALLBACK AboutDlgProc(HWND hwnd, UINT Message, WPARAM wParam, LPARAM lParam)
-{
-    switch (Message)
-    {
+        case IDCANCEL: {
+            PostQuitMessage(0);
+            break;
+        }
+        }
+        return 0;
+    }
+                  /* Perform initialization after all child windows have been created */
     case WM_INITDIALOG: {
 
-        SegmentDisplay_init(&l_scoreBoard,
-            2U,   /* 4 "segments" (digits 0-3) */
-            10U); /* 10 bitmaps (for 0-9 states) */
-        SegmentDisplay_initSegment(&l_scoreBoard, 0U, IDC_seg0);
-        SegmentDisplay_initSegment(&l_scoreBoard, 1U, IDC_seg1);
-        SegmentDisplay_initBitmap(&l_scoreBoard,
-            0U, LoadBitmap(l_hInst, MAKEINTRESOURCE(IDB_BITMAP1)));
-        SegmentDisplay_initBitmap(&l_scoreBoard,
-            1U, LoadBitmap(l_hInst, MAKEINTRESOURCE(IDB_BITMAP2)));
-        SegmentDisplay_initBitmap(&l_scoreBoard,
-            2U, LoadBitmap(l_hInst, MAKEINTRESOURCE(IDB_BITMAP3)));
-        SegmentDisplay_initBitmap(&l_scoreBoard,
-            3U, LoadBitmap(l_hInst, MAKEINTRESOURCE(IDB_BITMAP4)));
-        SegmentDisplay_initBitmap(&l_scoreBoard,
-            4U, LoadBitmap(l_hInst, MAKEINTRESOURCE(IDB_BITMAP5)));
-        SegmentDisplay_initBitmap(&l_scoreBoard,
-            5U, LoadBitmap(l_hInst, MAKEINTRESOURCE(IDB_BITMAP7)));
-        SegmentDisplay_initBitmap(&l_scoreBoard,
-            6U, LoadBitmap(l_hInst, MAKEINTRESOURCE(IDB_BITMAP8)));
-        SegmentDisplay_initBitmap(&l_scoreBoard,
-            7U, LoadBitmap(l_hInst, MAKEINTRESOURCE(IDB_BITMAP9)));
-        SegmentDisplay_initBitmap(&l_scoreBoard,
-            8U, LoadBitmap(l_hInst, MAKEINTRESOURCE(IDB_BITMAP10)));
-        SegmentDisplay_initBitmap(&l_scoreBoard,
-            9U, LoadBitmap(l_hInst, MAKEINTRESOURCE(IDB_BITMAP11)));
-
-        return 0;
-    }
-    case WM_COMMAND:
-        switch (LOWORD(wParam))
-        {
-        case IDOK:
-            count++;
-            update_seg(count);
-            break;
-            break;
-        case IDCANCEL:
-            EndDialog(hwnd, IDCANCEL);
-            break;
-        }
-        break;
-
-    case WM_KEYDOWN: {
-        switch (wParam) {
-        case VK_SPACE:
-            count++;
-            update_seg(count);
-            break;
-        }
         return 0;
     }
 
-    default:
-        return FALSE;
-    }
-    return TRUE;
-}
-
-
-
-LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
-{
-    switch (uMsg) {
-        /* Perform initialization after all child windows have been created */
-
-    case WM_DESTROY:
-    {
-        int ret = DialogBoxA(GetModuleHandle(NULL),
-            (LPCSTR)MAKEINTRESOURCE(IDD_DIALOG1), hwnd, AboutDlgProc);
-
-        if (ret == IDOK) {
-            MessageBox(hwnd, L"Dialog exited with IDOK.", L"Notice",
-                MB_OK | MB_ICONINFORMATION);
-        }
-        else if (ret == IDCANCEL) {
-            MessageBox(hwnd, L"Dialog exited with IDCANCEL.", L"Notice",
-                MB_OK | MB_ICONINFORMATION);
-        }
-        else if (ret == -1) {
-            MessageBox(hwnd, L"Dialog failed!", L"Error",
-                MB_OK | MB_ICONINFORMATION);
-        }
-    }
+    case WM_DESTROY: {
         PostQuitMessage(0);
         return 0;
-    case WM_PAINT:
-    {
-        PAINTSTRUCT ps;
-        HDC hdc = BeginPaint(hwnd, &ps);
-
-        // All painting occurs here, between BeginPaint and EndPaint.
-
-        FillRect(hdc, &ps.rcPaint, (HBRUSH)(COLOR_WINDOW + 1));
-
-        EndPaint(hwnd, &ps);
-    }
-    return 0;
     }
 
-    return DefWindowProc(hwnd, uMsg, wParam, lParam);
+    }
+    return DefWindowProc(hWnd, iMsg, wParam, lParam);
 }
 
